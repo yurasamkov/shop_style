233f1a8800bed99a6a320a1bca08a6a4ae4b0922
Yura Samkov <y.samkoff@ukr.net>
2015-10-15 23:36:23 +0300

    Initial shop style

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShopStyle::Application.config.secret_key_base = '8551bf703ff8c51b3883d857a039f5f0ff5475d4942e8dc69fe934734fb6ddf8fe619edcfbdd0852278afd7ab2e482abaaa12373fd79adaf9007bc95f7267fd4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShopStyle::Application.config.session_store :cookie_store, key: '_shop_style_session'
