9fc8931812fbef84318e6c236aa1491b10f592cb
Yura Samkov <y.samkoff@ukr.net>
2015-10-24 23:08:00 +0300

    git add .!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ShopStyle::Application.routes.draw do
+  root "pages#home"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
